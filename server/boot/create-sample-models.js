58baf8b1e4e20e4ded89037d84f663b7b812061b
Herberts Cruz <herbertscruz@gmail.com>
2017-03-23 00:00:39 -0300

    - Evolução no mapeamento das entidades do modelo; - Implantação do migrate de criação das tabelas e da massa de dados;

---
+++ server/boot/create-sample-models.js
@@ -0,0 +1,52 @@
+'use strict';
+
+require('rootpath')();
+
+var fs = require('fs');
+var path = require("path");
+var upperCamelCase = require('uppercamelcase');
+
+module.exports = function (app) {
+    //Adiciona o diretório das amostras
+    var pathDir = path.join('server', 'samples');
+    //Lê todos os arquivos do diretório
+    fs.readdir(pathDir, function (err, files) {
+        if (err) throw err;
+        //Navega nos arquivos do diretório
+        for (var i in files) {
+            if (typeof files[i] === 'string') {
+                //Se for um arquivo do tipo JSON
+                if (path.extname(files[i]) === '.json') {
+                    //Obtêm o path do arquivo
+                    var filename = path.join(pathDir, path.basename(files[i]));
+                    //Obtêm o nome do modelo
+                    var modelname = upperCamelCase(path.basename(files[i], '.json'));
+                    //Cria a tabela e a popula com os dados do arquivo
+                    _create(app, filename, modelname);
+                }
+            }
+        }
+    });
+};
+
+/**
+ * Cria a tabela e a popula com os dados do arquivo.
+ *
+ * @param app
+ * @param filename
+ * @param modelname
+ * @private
+ */
+function _create (app, filename, modelname) {
+    //Inicia o processo importação da massa de dados
+    app.dataSources.db.automigrate(modelname, function (err) {
+        if (err) throw err;
+        //Lê o arquivo json
+        var json = require(filename);
+        //Adiciona os dados na base de dados
+        app.models[modelname].create(json, function (err, model) {
+            if (err) throw err;
+            console.log('Models created: \n', model);
+        });
+    });
+}
